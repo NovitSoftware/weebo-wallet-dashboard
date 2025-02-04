a5025e298acb8400d410786b09b0dc7dc9b3b3df
Ramon-novit <ramon.lugones@novit.com.ar>
2025-02-04 12:40:59 -0300

    clientes en riesgos (90 dias)

---
+++ Tests/WEEBO-10020-ClientesenRiesgo(estácercaalos90díassinusarelprograma).sql
@@ -0,0 +1,11 @@
+-- 1.12 Clientes en Riesgo (está cerca a los 90 días sin usar el programa): Número total, promedio y variación frente periodo anterior según periodo seleccionado.
+-- Gráfica: Sin chart.
+-- total
+SELECT COUNT(DISTINCT aw.KeyAfiliado) AS TotalClientesEnRiesgo
+FROM dwh.Fact_AfiliacionWebo aw
+WHERE DATEDIFF(DAY, aw.FechaActivo, GETDATE()) BETWEEN 75 AND 90;
+
+-- promedio 
+SELECT avg(DISTINCT aw.KeyAfiliado) AS TotalClientesEnRiesgo
+FROM dwh.Fact_AfiliacionWebo aw
+WHERE DATEDIFF(DAY, aw.FechaActivo, GETDATE()) BETWEEN 75 AND 90;
