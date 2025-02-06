512a25efab2979772b5640e4c2de500095fc9fd9
Ramon-novit <ramon.lugones@novit.com.ar>
2025-02-06 10:13:14 -0300

    intento de reparar conflictos

--- Tests/WEEBO-10034-StickersporTarjeta.sql
+++ Tests/WEEBO-10034-StickersporTarjeta.sql
@@ -1,6 +1,8 @@
 -- 3.2 Stickers por Tarjeta: Promedio de stickers otorgados por el total de tarjetas activas.
 -- Gráfica: Sin chart. 
 
-select avg(s.totales) as 'Promedio otorgados por tajetas activas' from dbo.tarjeta as t
-inner join dbo.sticker as s on s.fktarjeta = t.codigo
-where t.activo = 's';
+SELECT 
+    SUM(COALESCE(fp.StickersEntregados, 0)) * 1.0 / NULLIF(SUM(fp.TarjetasActivadas), 0) AS PromedioStickersPorTarjeta
+FROM dwh.Fact_Planes fp;
+
+
