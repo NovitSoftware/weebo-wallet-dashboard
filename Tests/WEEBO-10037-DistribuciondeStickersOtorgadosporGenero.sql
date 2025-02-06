e344361c56b0c45ef24291e4b0a65a127c13ada8
Ramon-novit <ramon.lugones@novit.com.ar>
2025-02-06 13:07:54 -0300

    Sticker entregados por variable demografica segun periodo de tiempo

--- Tests/WEEBO-10037-DistribuciondeStickersOtorgadosporGenero.sql
+++ Tests/WEEBO-10037-DistribuciondeStickersOtorgadosporGenero.sql
@@ -1,6 +1,10 @@
 -- 3.5 Distribución de Stickers Otorgados por Género: Número total de stickers otorgado por género.
 -- Gráfica: Pie chart. 
 
-select sum(s.totales) as 'Sticker otorgados', u.genero from dbo.usuario as u
-inner join dbo.sticker as s on u.idusuario = s.fkusuario
-group by u.genero
+SELECT 
+    da.Genero,
+    SUM(COALESCE(fc.StickersEntregados, 0)) AS TotalStickersOtorgados
+FROM dwh.Fact_Cupon fc
+JOIN dwh.Dim_Afiliado da ON fc.KeyAfiliado = da.KeyAfiliado
+GROUP BY da.Genero
+ORDER BY TotalStickersOtorgados DESC;
